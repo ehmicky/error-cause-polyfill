9237504ba3e204dc0dbf7c76eacdae3eda3b4cf5
ehmicky <ehmicky@gmail.com>
2022-06-12 21:22:38 +0200

    Refactoring

--- test/matrix/not_v8.js
+++ test/matrix/not_v8.js
@@ -1,9 +1,8 @@
 import { defineAllTests } from '../helpers/tests/main.js'
 
-// eslint-disable-next-line fp/no-delete
-delete Error.captureStackTrace
-
 // Run tests with the Error types without any polyfill nor ponyfill.
 // But mimicking running not in V8, since V8 has some specific behavior related
 // to `Error`.
+// eslint-disable-next-line fp/no-delete
+delete Error.captureStackTrace
 defineAllTests(globalThis, false)
--- test/matrix/ponyfill.js
+++ test/matrix/ponyfill.js
@@ -3,5 +3,4 @@ import { getErrors } from 'error-cause-polyfill'
 import { defineAllTests } from '../helpers/tests/main.js'
 
 // Run tests with the Error types with the ponyfills
-const PonyfillErrors = getErrors()
-defineAllTests(PonyfillErrors, true)
+defineAllTests(getErrors(), true)
