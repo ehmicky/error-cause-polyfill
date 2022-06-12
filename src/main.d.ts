14a69222eb3a1c0fa6159d6d51cade15a56795ed
ehmicky <ehmicky@gmail.com>
2022-06-12 21:57:15 +0200

    Add types

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,7 +1,39 @@
+export type UndoPolyfill = () => void
+
 /**
  *
  * @example
  * ```js
  * ```
  */
-export function polyfill(): void
+export function polyfill(): UndoPolyfill
+
+export interface Errors {
+  Error: Error
+  ReferenceError: Error
+  TypeError: Error
+  SyntaxError: Error
+  RangeError: Error
+  URIError: Error
+  EvalError: Error
+  /**
+   * `undefined` if the system does not support `AggregateError`
+   */
+  AggregateError?: Error
+}
+
+/**
+ *
+ * @example
+ * ```js
+ * ```
+ */
+export function getErrors(): Errors
+
+/**
+ *
+ * @example
+ * ```js
+ * ```
+ */
+export function hasSupport(): boolean
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,6 +1,31 @@
-import {polyfill} from 'error-cause-polyfill'
-import { expectType, expectError } from 'tsd'
+import {
+  polyfill,
+  UndoPolyfill,
+  getErrors,
+  Errors,
+  hasSupport,
+} from 'error-cause-polyfill'
+import {
+  expectType,
+  expectAssignable,
+  expectNotAssignable,
+  expectError,
+} from 'tsd'
 
-expectType<void>(polyfill())
-
+const undoPolyfillFunc = polyfill()
 expectError(polyfill(true))
+expectType<UndoPolyfill>(undoPolyfillFunc)
+expectAssignable<UndoPolyfill>(() => {})
+expectNotAssignable<UndoPolyfill>((_: boolean) => {})
+undoPolyfillFunc()
+expectError(undoPolyfillFunc(true))
+
+const errors = getErrors()
+expectError(getErrors(true))
+expectType<Errors>(errors)
+expectType<Error>(errors.Error)
+expectType<Error>(errors.TypeError)
+expectType<Error | undefined>(errors.AggregateError)
+
+expectType<boolean>(hasSupport())
+expectError(hasSupport(true))
