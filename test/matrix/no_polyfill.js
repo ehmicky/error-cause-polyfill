6f54944fa1d714a916fb51c3a545d18d8ab7a2d0
ehmicky <ehmicky@gmail.com>
2022-06-12 21:22:34 +0200

    Fix comment

--- test/matrix/no_polyfill.js
+++ test/matrix/no_polyfill.js
@@ -1,6 +1,4 @@
 import { defineAllTests } from '../helpers/tests/main.js'
 
 // Run tests with the Error types without any polyfill nor ponyfill
-/* jscpd:ignore-start */
 defineAllTests(globalThis, false)
-/* jscpd:ignore-end */
