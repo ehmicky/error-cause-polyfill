0b791839725fc79b6c88dd934902ff8036ad5e2f
ehmicky <ehmicky@gmail.com>
2022-06-12 17:58:57 +0200

    Use children errors

--- test/helpers/main.js
+++ test/helpers/main.js
@@ -13,23 +13,50 @@ export const defineAllTests = function (getTypes) {
 
 const defineTests = function ({ name, args, getTypes }) {
   const { ErrorType, OriginalAnyError } = getTypes(name)
+  const ChildError = getChildError(ErrorType)
+  const GrandChildError = getChildError(ChildError)
+  const ErrorTypes = { ErrorType, ChildError, GrandChildError }
+  Object.entries(ErrorTypes).forEach(([ErrorTypeName, ErrorTypeA]) => {
+    const title = `| ${name} | ${ErrorTypeName}`
+    defineTestsSeries({ title, args, ErrorType: ErrorTypeA, OriginalAnyError })
+  })
+}
 
+const getChildError = function (ParentError) {
+  // eslint-disable-next-line fp/no-class, unicorn/custom-error-definition
+  class ChildError extends ParentError {}
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(ChildError.prototype, 'name', {
+    value: ChildError.name,
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  })
+  return ChildError
+}
+
+const defineTestsSeries = function ({
+  title,
+  args,
+  ErrorType,
+  OriginalAnyError,
+}) {
   const message = 'test'
   const error = new ErrorType(...args, message)
 
-  test(`Is instance of original base Error | ${name}`, (t) => {
+  test(`Is instance of original base Error ${title}`, (t) => {
     t.true(error instanceof OriginalErrors.Error)
   })
 
-  test(`Is instance of polyfill base Error | ${name}`, (t) => {
+  test(`Is instance of polyfill base Error ${title}`, (t) => {
     t.true(error instanceof Errors.Error)
   })
 
-  test(`Is instance of original Error | ${name}`, (t) => {
+  test(`Is instance of original Error ${title}`, (t) => {
     t.true(error instanceof OriginalAnyError)
   })
 
-  test(`Is instance of polyfill Error | ${name}`, (t) => {
+  test(`Is instance of polyfill Error ${title}`, (t) => {
     t.true(error instanceof ErrorType)
   })
 }
