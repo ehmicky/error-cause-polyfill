45774043f55aa09b2b45c0db936fc1b3220a2906
ehmicky <ehmicky@gmail.com>
2022-06-12 21:33:42 +0200

    Add tests

--- test/polyfill.js
+++ test/polyfill.js
@@ -1,5 +1,7 @@
 import test from 'ava'
 import { polyfill, hasSupport } from 'error-cause-polyfill'
+// eslint-disable-next-line n/file-extension-in-import
+import OtherPolyfillError from 'error-cause/Error/implementation'
 
 import { getOriginalErrors } from './helpers/types.js'
 
@@ -40,3 +42,32 @@ test.serial('polyfill() can be done twice', (t) => {
   undoPolyfill()
   t.is(globalThis.Error, originalErrors.Error)
 })
+
+test.serial('Can polyfill after another polyfill', (t) => {
+  t.is(Error, originalErrors.Error)
+  setOtherPolyfill()
+  t.is(Error, OtherPolyfillError)
+  const undoPolyfill = polyfill()
+  undoPolyfill()
+  t.is(Error, OtherPolyfillError)
+  unsetOtherPolyfill()
+  t.is(Error, originalErrors.Error)
+})
+
+const setOtherPolyfill = function () {
+  setGlobalError(OtherPolyfillError)
+}
+
+const unsetOtherPolyfill = function () {
+  setGlobalError(originalErrors.Error)
+}
+
+const setGlobalError = function (value) {
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(globalThis, 'Error', {
+    value,
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  })
+}
