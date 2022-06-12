c578dbe4dd01b1184e0b229e64b9c1948c094fdb
ehmicky <ehmicky@gmail.com>
2022-06-12 19:41:48 +0200

    Refactoring

--- test/helpers/assert.js
+++ test/helpers/assert.js
@@ -1,40 +1,10 @@
-/* eslint-disable max-depth */
 import assert from 'assert/strict'
 
-import { getErrors, test, polyfill } from 'error-cause-polyfill'
+import { test, polyfill } from 'error-cause-polyfill'
 
 import { getOriginalErrors } from './types.js'
 
 const OriginalErrors = getOriginalErrors()
-const PonyfillErrors = getErrors()
-
-const ALL_ERRORS = [
-  [OriginalErrors.Error, PonyfillErrors.Error],
-  [OriginalErrors.ReferenceError, PonyfillErrors.ReferenceError],
-  [OriginalErrors.TypeError, PonyfillErrors.TypeError],
-  [OriginalErrors.SyntaxError, PonyfillErrors.SyntaxError],
-  [OriginalErrors.RangeError, PonyfillErrors.RangeError],
-  [OriginalErrors.URIError, PonyfillErrors.URIError],
-  [OriginalErrors.EvalError, PonyfillErrors.EvalError],
-  ...(AggregateError === undefined
-    ? []
-    : [[OriginalErrors.AggregateError, PonyfillErrors.AggregateError]]),
-]
-
-// eslint-disable-next-line fp/no-loops
-for (const [OriginalAnyError, PonyfillAnyError] of ALL_ERRORS) {
-  if (OriginalAnyError.name === 'Error') {
-    const oldStackTraceLimit = PonyfillAnyError.stackTraceLimit
-    // eslint-disable-next-line fp/no-mutation
-    PonyfillAnyError.stackTraceLimit = 0
-    assert.equal(
-      new PonyfillAnyError('').stack,
-      PonyfillAnyError.prototype.name,
-    )
-    // eslint-disable-next-line fp/no-mutation
-    PonyfillAnyError.stackTraceLimit = oldStackTraceLimit
-  }
-}
 
 const hasPolyfill = function () {
   return globalThis.Error !== OriginalErrors.Error
@@ -47,4 +17,3 @@ assert.equal(!hasPolyfill(), testWithoutPolyfill)
 assert(test())
 undoPolyfill()
 assert(!hasPolyfill())
-/* eslint-enable max-depth */
