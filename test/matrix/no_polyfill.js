62054607b19728f591f0d665e5b0e452f7aaef9e
ehmicky <ehmicky@gmail.com>
2022-06-12 20:01:04 +0200

    Fix linting

--- test/matrix/no_polyfill.js
+++ test/matrix/no_polyfill.js
@@ -5,6 +5,7 @@ import { defineAllTests } from '../helpers/tests/main.js'
 const supportsCause = hasSupport()
 
 // Run tests with the Error types without any polyfill nor ponyfill
+/* jscpd:ignore-start */
 defineAllTests(
   (name) => ({
     PonyfillAnyError: globalThis[name],
@@ -16,3 +17,4 @@ defineAllTests(
     supportsCause,
   },
 )
+/* jscpd:ignore-end */
