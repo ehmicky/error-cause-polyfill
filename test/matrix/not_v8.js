253a9be2cec1417a6396516beb0b4d7e5040a15c
ehmicky <ehmicky@gmail.com>
2022-06-12 21:27:50 +0200

    Improve test coverage

--- test/matrix/not_v8.js
+++ test/matrix/not_v8.js
@@ -1,8 +1,9 @@
+import { getErrors } from 'error-cause-polyfill'
+
 import { defineAllTests } from '../helpers/tests/main.js'
 
-// Run tests with the Error types without any polyfill nor ponyfill.
-// But mimicking running not in V8, since V8 has some specific behavior related
+// Mimics running not in V8, since V8 has some specific behavior related
 // to `Error`.
 // eslint-disable-next-line fp/no-delete
 delete Error.captureStackTrace
-defineAllTests(globalThis, false)
+defineAllTests(getErrors(), true)
