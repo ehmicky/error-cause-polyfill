44af001ca6f8c36c2251d347ed8cb54a0418386f
ehmicky <ehmicky@gmail.com>
2022-06-19 23:18:02 +0200

    Refactoring

--- src/types.js
+++ src/types.js
@@ -1,16 +1,5 @@
-// We do not polyfill `AggregateError`. If unsupported, those exports are
-// `undefined`.
+// We do not polyfill `AggregateError` unless supported.
 // Any `AggregateError` polyfill must be loaded first.
-const hasAggregateError = function () {
-  try {
-    // eslint-disable-next-line no-unused-expressions
-    AggregateError
-    return true
-  } catch {
-    return false
-  }
-}
-
 export const ERROR_TYPES = [
   { name: 'Error', shouldProxy: true, argsLength: 1 },
   { name: 'ReferenceError', shouldProxy: false, argsLength: 1 },
@@ -19,7 +8,7 @@ export const ERROR_TYPES = [
   { name: 'RangeError', shouldProxy: false, argsLength: 1 },
   { name: 'URIError', shouldProxy: false, argsLength: 1 },
   { name: 'EvalError', shouldProxy: false, argsLength: 1 },
-  ...(hasAggregateError()
+  ...('AggregateError' in globalThis
     ? [{ name: 'AggregateError', shouldProxy: false, argsLength: 2 }]
     : []),
 ]
--- test/helpers/types.js
+++ test/helpers/types.js
@@ -1,27 +1,3 @@
-// We do not polyfill `AggregateError`. If unsupported, those exports are
-// `undefined`.
-// Any `AggregateError` polyfill must be loaded first.
-const hasAggregateError = function () {
-  try {
-    // eslint-disable-next-line no-unused-expressions
-    AggregateError
-    return true
-  } catch {
-    return false
-  }
-}
-
-export const ERROR_TYPES = [
-  'Error',
-  'ReferenceError',
-  'TypeError',
-  'SyntaxError',
-  'RangeError',
-  'URIError',
-  'EvalError',
-  ...(hasAggregateError() ? ['AggregateError'] : []),
-]
-
 // Retrieve original errors before polyfilling
 export const getOriginalErrors = function () {
   return Object.fromEntries(ERROR_TYPES.map(getOriginalAnyError))
@@ -30,3 +6,16 @@ export const getOriginalErrors = function () {
 const getOriginalAnyError = function (name) {
   return [name, globalThis[name]]
 }
+
+// We do not polyfill `AggregateError` unless supported.
+// Any `AggregateError` polyfill must be loaded first.
+export const ERROR_TYPES = [
+  'Error',
+  'ReferenceError',
+  'TypeError',
+  'SyntaxError',
+  'RangeError',
+  'URIError',
+  'EvalError',
+  ...('AggregateError' in globalThis ? ['AggregateError'] : []),
+]
