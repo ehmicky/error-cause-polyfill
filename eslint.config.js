735c7fe2a11808f84796bc1f6086febd27c1d343
ehmicky <ehmicky@gmail.com>
2024-10-27 14:15:55 +0000

    Fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,11 @@
-export { default } from '@ehmicky/eslint-config'
+import eslintConfig from '@ehmicky/eslint-config'
+
+export default [
+  ...eslintConfig,
+  {
+    rules: {
+      // This reports this module itself as an unnecessary polyfill
+      'unicorn/no-unnecessary-polyfills': 0,
+    },
+  },
+]
